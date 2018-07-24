59458ba6d27a8f7cba0ea8bf2539c8bbda46efb3
Malik BEN KIRANE <mben-kir@e2r11p2.42.fr>
2018-07-24 22:47:01 +0200

    update read_map and read_map_line sources

--- src/read_map.c
+++ src/read_map.c
@@ -1,21 +1,17 @@
-/*
-** TODO think if it's necessary to use int type to count
-** the biggest square size.
-**
-** e.g. tiny / short (1 or 2 bytes could suffy)
-**
-** get exit calls to the right exit status specification
-*/
-
 #include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "macros.h"
+#include "error.h"
 #include "grid.h"
 #include "constants.h"
+#include "grid.h"
+#include "grid_lines.h"
+#include "free.h"
 #include "ft.h"
+#include "read.h"
 
-char	*ft_realloc(char *current, char *to_add)
+char			*ft_realloc(char *current, char *to_add)
 {
 	char	*str;
 	int		current_len;
@@ -42,49 +38,85 @@ char	*ft_realloc(char *current, char *to_add)
 	return (str);
 }
 
-t_grid 			*read_map(int fildes)
+/*
+** TODO TESTME t_grid *read_map(int fildes)
+*/
+t_read_cue	*read_map(int fildes)
 {
 	t_grid_desc	*grid_desc;
-	char		*map_str;
+	t_grid		*grid;
+	t_read_cue	*read_cue;
 
-	/*
-	** XXX
-	** - malloc and return ref
-	** must exit if error
-	*/
 	if ((grid_desc = read_description(fildes)) == NULL)
 		exit_with_map_error();
-	if ((map_str = read_file(fildes) == NULL))
+	if ((grid = (t_grid *)malloc(sizeof(t_grid))) == NULL)
+	{
+		free(grid_desc);
+		return (NULL);
+	}
+	if ((read_cue = read_file(fildes, grid_desc, grid)) == NULL)
 		exit_with_map_error();
-	/*
-	** TODO test read_file re-written
-	*/.
-	return (NULL);
+	read_cue->cue_line = grid->first_line;
+	free(read_cue->buffer);
+	return (read_cue);
 }
 
-char			*read_file(int fildes, t_grid *grid)
+t_read_cue		*read_file(int fildes, t_grid_desc *grid_desc, t_grid *grid)
 {
-	char		*buf;
-	int			size;
-	char		*str;
-	t_grid_desc	*grid_desc;
+	t_read_cue	*r_cue;
 
-	if (!(buf = (char*)malloc(sizeof(*buf) * BUFFER_SIZE + 1)))
-		return (NULL)
-	total_size = 0;
-	while ((size = read(fildes, buf, BUFFER_SIZE)))
+	if ((r_cue = (t_read_cue *)malloc(sizeof(t_read_cue))) == NULL)
+		return (NULL);
+	if ((r_cue->buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1)) == NULL)
+		return (free_objects(NULL, NULL, r_cue));
+	if ((grid->first_line = init_grid_line()) == NULL)
+		return (free_objects(r_cue->buffer, NULL, NULL));
+	r_cue->cue_line = grid->first_line;
+	r_cue->fildes = fildes;
+	if (read_file_lines(grid_desc, grid, r_cue) == NULL)
+		return (free_objects(r_cue->buffer, grid->first_line, r_cue));
+	if (*(r_cue->cue) != '\0')
+		return (free_objects(r_cue->head, grid->first_line, NULL));
+	if (map_count_lines(r_cue->head) != grid_desc->nline)
+		return (free_objects(r_cue->head, grid->first_line, NULL));
+	free(r_cue->buffer);
+	return (r_cue);
+}
+
+
+void 			*read_file_lines(t_grid_desc *grid_desc, t_grid *grid, t_read_cue *r_cue)
+{
+	int			size;
+
+	while ((size = read(r_cue->fildes, r_cue->buffer, BUFFER_SIZE)) > 0)
 	{
-		while (size > 0 && !iseol(buf))
+		r_cue->buffer[size] = '\0';
+		r_cue->cue = ft_realloc(r_cue->cue, r_cue->buffer);
+		while (size > 0 && !iseol(r_cue->buffer))
 		{
-			size = read(fildes, buf, BUFFER_SIZE);
-			buf[size] = '\0';
-			str = ft_realloc(str, buf);
+			size = read(r_cue->fildes, r_cue->buffer, BUFFER_SIZE);
+			r_cue->buffer[size] = '\0';
+			r_cue->cue = ft_realloc(r_cue->cue, r_cue->buffer);
+			r_cue->head = r_cue->cue;
 		}
-		if (size == 0 && !iseol(buf))
-			return (NULL);
-		if (!read_map_first_line(&str, grid_desc, grid))
-			return (NULL);
+		if (size == 0 && !iseol(r_cue->buffer))
+			return (free_objects(r_cue->head, grid->first_line, NULL));
+		if (!read_map_line(&(r_cue->cue), grid_desc, r_cue->cue_line))
+			return (free_objects(r_cue->head, grid->first_line, NULL));
+		if ((grid->first_line->next_line = init_grid_line()) == NULL)
+			return (free_objects(r_cue->head, grid->first_line, NULL));
+		r_cue->cue_line = grid->first_line->next_line;
 	}
-	(*str)[total_size] = '\0';
-	return (str);
+	return ((void *)1);
+}
+
+t_dyn_int		map_count_lines(char *tmp)
+{
+	t_dyn_int nline;
+
+	nline = 0;
+	while (*tmp)
+		if (*tmp++ == '\n')
+			nline++;
+	return (nline);
 }
--- src/read_map_line.c
+++ src/read_map_line.c
@@ -1,5 +1,6 @@
 #include "constants.h"
 #include "read.h"
+#include "grid_lines.h"
 
 t_bool			iseol(char *str)
 {
@@ -10,26 +11,10 @@ t_bool			iseol(char *str)
 	return (FALSE);
 }
 
-void			update_grid_line(char *cur, t_grid_desc *grid_desc, t_grid_line *line)
-{
-	int len;
-
-	len = 0;
-	while(*cur && *cur != '\n')
-	{
-		if (*cur == grid_desc->empty_c)
-			line->grid_line[len] = 1;
-		else
-			line->grid_line[len] = 0;
-		cur++;
-		len++;
-	}
-}
-
 /*
 ** TODO TESTME
 */
-t_bool			read_map_line(char **str, t_grid *grid,
+t_bool			read_map_line(char **str,
 		t_grid_desc *grid_desc, t_grid_line *line)
 {
 	t_dyn_int len;
