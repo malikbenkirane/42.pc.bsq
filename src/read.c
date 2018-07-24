19c83815a34fd1a7021064465635671b76006a22
Malik BEN KIRANE <mben-kir@e2r11p2.42.fr>
2018-07-24 18:57:00 +0200

    update read.c

--- src/read.c
+++ src/read.c
@@ -10,26 +10,39 @@ t_bool			iseol(char *str)
 	return (FALSE);
 }
 
+void			update_grid_line(char *cur, t_grid_desc *grid_desc, t_grid_line *line)
+{
+	int len;
+
+	len = 0;
+	while(*cur && *cur != '\n')
+	{
+		if (*cur == grid_desc->empty_c)
+			line->grid_line[len] = 1;
+		else
+			line->grid_line[len] = 0;
+		cur++;
+		len++;
+	}
+}
+
 /*
 ** TODO TESTME
 */
-t_bool			read_map_line(char **str, t_grid_desc *grid_desc, t_grid *grid)
+t_bool			read_map_line(char **str, t_grid *grid,
+		t_grid_desc *grid_desc, t_grid_line *line)
 {
-	unsigned int len;
+	t_dyn_int len;
 	char *cur;
 
 	cur = *str;
 	len = 0;
 	while(*cur && *cur != '\n')
 	{
-		if (*cur != grid_desc->empty_c ||*cur != grid_desc->trap_c)
+		if (*cur != grid_desc->empty_c || *cur != grid_desc->trap_c)
 			return (FALSE);
-		if (*cur == grid_desc->empty_c)
-			grid->grid[len] = 1;
-		else
-			grid->grid[len] = 0;
-		cur++;
 		len++;
+		cur++;
 	}
 	if (len == 0)
 		return (FALSE);
@@ -37,6 +50,7 @@ t_bool			read_map_line(char **str, t_grid_desc *grid_desc, t_grid *grid)
 		grid_desc->ncol = len;
 	else if (len != grid_desc->ncol)
 		return (FALSE);
+	update_grid_line(*str, grid_desc, line);
 	*str = *str + len + 1;
 	return (TRUE);
 }
