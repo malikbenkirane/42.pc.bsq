30b54722f442ea2b709c176e7c3822ad98547fc8
Malik BEN KIRANE <mben-kir@e2r11p2.42.fr>
2018-07-24 22:46:35 +0200

    update main.c with relevant TODOS

--- src/main.c
+++ src/main.c
@@ -8,12 +8,18 @@ int		main(int argc, char **argv)
 	if (argc == 1)
 	{
 		read_map(stdin->_file);
+		/*
+		** TODO change ft_pustr loop with one write call
+		*/
 		ft_putstr(solve_map(grid));
 	}
 	index = 1;
 	while (index < argc)
 	{
 		grid = read_map(open(argv[index++]));
+		/*
+		** TODO change ft_pustr loop with one write call
+		*/
 		ft_putstr(solve_map(grid));
 	}
 }
