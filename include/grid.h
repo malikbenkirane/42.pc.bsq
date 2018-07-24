5b1cafb7faae58a02c7ccb22a6da3eb5f3854179
Malik BEN KIRANE <mben-kir@e2r11p2.42.fr>
2018-07-24 18:06:37 +0200

    update include/grid.h
    will update structures that represent the grid

--- include/grid.h
+++ include/grid.h
@@ -6,26 +6,32 @@
 #include "constants.h"
 #include "macros.h"
 
-typedef	struct		s_grid
+typedef	struct			s_grid_line
 {
-	unsigned int	*grid;
-	unsigned int	width;
-	unsigned int	height;
-}					t_grid;
+	unsigned int		*grid_line;
+	struct s_grid_chain	*next_line;
+}						t_grid_line;
 
-typedef	struct		s_grid_desc
+typedef	struct			s_grid
 {
-	unsigned int	nline;
-	unsigned int	ncol;
-	char			empty_c;
-	char			trap_c;
-	char			fill_c;
-}					t_grid_desc;
+	t_grid_line			*first_line;
+	unsigned int		width;
+	unsigned int		height;
+}						t_grid;
 
-char				*is_valid_description(short *length, int fildes);
-char				*read_file(int fildes, t_grid *grid)
-char				*ft_realloc(char *current, char *to_add)
-t_grid				*read_map(int fildes)
-t_grid_desc			*read_description(int fildes);
+typedef	struct			s_grid_desc
+{
+	unsigned int		nline;
+	unsigned int		ncol;
+	char				empty_c;
+	char				trap_c;
+	char				fill_c;
+}						t_grid_desc;
+
+char					*is_valid_description(short *length, int fildes);
+char					*read_file(int fildes, t_grid *grid)
+char					*ft_realloc(char *current, char *to_add)
+t_grid					*read_map(int fildes)
+t_grid_desc				*read_description(int fildes);
 
 #endif
