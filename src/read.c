4c05eacb8447bc6b8b61db5cf91a686a48415e5f
Malik BEN KIRANE <mben-kir@e2r11p2.42.fr>
2018-07-24 15:47:05 +0200

    add read.c method read_map_line to read map line method iseol checks if end of line

---
+++ src/read.c
@@ -0,0 +1,48 @@
+#include "constants.h"
+#include "read.h"
+
+/*
+** 1111111111111011111111
+** 1111111111111111111111
+** 1
+*/
+
+t_bool			iseol(char *str)
+{
+	while (*str && *str != '\n')
+		str++;
+	if (*str == '\n')
+		return (TRUE);
+	return (FALSE);
+}
+
+/*
+** TODO TESTME
+*/
+t_bool			read_map_line(char **str, t_grid_desc *grid_desc, t_grid *grid)
+{
+	unsigned int len;
+	char *cur;
+
+	cur = *str;
+	len = 0;
+	while(*cur && *cur != '\n')
+	{
+		if (*cur != grid_desc->empty_c ||*cur != grid_desc->trap_c)
+			return (FALSE);
+		if (*cur == grid_desc->empty_c)
+			grid->grid[len] = 1;
+		else
+			grid->grid[len] = 0;
+		cur++;
+		len++;
+	}
+	if (len == 0)
+		return (FALSE);
+	if (grid_desc->ncol == 0)
+		grid_desc->ncol = len;
+	else if (len != grid_desc->ncol)
+		return (FALSE);
+	*str = *str + len + 1;
+	return (TRUE);
+}
