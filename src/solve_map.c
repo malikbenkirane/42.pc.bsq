530c7215091690edf25a58e95944624e964990a5
Malik BEN KIRANE <mben-kir@e2r11p2.42.fr>
2018-07-25 15:37:39 +0200

    fix src/solve_map.c through compilation

--- src/solve_map.c
+++ src/solve_map.c
@@ -1,13 +1,23 @@
+#include "constants.h"
+#include "grid.h"
+#include "solve.h"
 #include "grid_lines.h"
 
-void		solve_map(t_grid *grid, char *map_str, t_grid_desc *grid_desc)
+void			solve_map(t_grid *grid, char *map_str, t_grid_desc *grid_desc)
 {
 	t_bsq_solution	*solution;
-	int 			offset;
-	int 			counter;
+	t_dyn_int		offset;
+	t_dyn_int		counter;
 	char			*ptr;
 
-	solution = solve_bsq(grid);
+	/*
+	** TODO look for calls of solve_map 
+	** (spot unsecure access to grid or grid_access)
+	*/
+	grid->width = grid_desc->ncol;
+	grid->height = grid_desc->nline;
+	if ((solution = solve_bsq(grid)) == NULL)
+		return ;
 	counter = 0;
 	ptr = map_str + solution->argopt;
 	while (counter < solution->opt)
@@ -18,8 +28,8 @@ void		solve_map(t_grid *grid, char *map_str, t_grid_desc *grid_desc)
 			ptr[offset] = grid_desc->fill_c;
 			offset--;
 		}
-		ptr = ptr - grid_desc->ncol;
-		counter ++;
+		ptr = ptr - grid_desc->ncol + 1;
+		counter++;
 	}
 }
 
@@ -27,23 +37,23 @@ t_bsq_solution	*solve_bsq(t_grid *grid)
 {
 	t_dyn_int 		i;
 	t_dyn_int 		j;
-	t_dyn_int 		*size;
+	t_dyn_int 		size;
 	t_bsq_solution	*solution;
 
 	i = 1;
 	j = 1;
-	if (solution = init_bsq_solution())
+	if ((solution = init_bsq_solution()) == NULL)
 		return (NULL);
-	while (i < grid->ncol && j <= grid->nline)
+	while (i < grid->width && j <= grid->height)
 	{
 		i = 1;
-		while (i < grid->ncol)
+		while (i < grid->width)
 		{
 			size = get_min(grid, i, j);
-			if (size > max)
+			if (size > solution->opt)
 			{
 				solution->opt = size;
-				solution->argopt = j * grid->ncol + i;
+				solution->argopt = j * grid->width + i;
 			}
 			set_opt_size(grid, i, j, size);
 			i++;
@@ -59,9 +69,9 @@ t_dyn_int		get_min(t_grid *grid, t_dyn_int i, t_dyn_int j)
 	t_dyn_int	above;
 	t_dyn_int	diag;
 
-	left = get_opt_square(grid, i - 1, j);
-	above = get_opt_square(grid, i, j - 1);
-	diag = get_opt_square(grid, i - 1, j - 1);
+	left = get_opt_size(grid, i - 1, j);
+	above = get_opt_size(grid, i, j - 1);
+	diag = get_opt_size(grid, i - 1, j - 1);
 	return (MIN_3(left, above, diag));
 }
 
