b549434dec158417cd4cc9b81707072a2fb1f119
Malik BEN KIRANE <mben-kir@e2r11p2.42.fr>
2018-07-25 15:36:38 +0200

    update include/grid_lines.h
    add get_opt_size and set_opt_size prototypes

--- include/grid_lines.h
+++ include/grid_lines.h
@@ -6,4 +6,7 @@
 t_grid_line		*init_grid_line();
 void			update_grid_line(char *cur, t_grid_desc *grid_desc, t_grid_line *line);
 
+t_dyn_int	get_opt_size(t_grid *grid, t_dyn_int posx, t_dyn_int posy);
+void		set_opt_size(t_grid *grid, t_dyn_int posx, t_dyn_int posy, t_dyn_int val);
+
 #endif
