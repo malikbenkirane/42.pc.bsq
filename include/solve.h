33d95e834e35e209c72a69069ef0abbd5d357100
Marine TRIZAC <m.trizac@gmail.com>
2018-07-25 13:53:40 +0200

    modification of prototype solve_bsq

--- include/solve.h
+++ include/solve.h
@@ -7,9 +7,7 @@ typedef struct	s_bsq_solution
 	t_dyn_int	argopt;
 }				t_bsq_solution;
 
-t_bsq_solution	*solve_bsq(
-		t_grid *grid,
-		t_dyn_int *size, t_dyn_int *posx, t_dyn_int *posy);
+t_bsq_solution	*solve_bsq(t_grid *grid, t_dyn_int *size);
 
 t_dyn_int		get_min(t_grid *grid, t_dyn_int i, t_dyn_int j);
 t_bsq_solution	*init_bsq_solution();
